af95b109d829806a01c6444a4d3db0e1e9bacf69
melROLL <60772441+melROLL@users.noreply.github.com>
2022-11-13 00:06:01 +0100

    update

--- create_release.py
+++ create_release.py
@@ -14,8 +14,8 @@ from datetime import date
 import tarfile
 import subprocess
 
-repo = "supermerill/SuperSlicer"
-program_name = "SuperSlicer"
+repo = "melROLL/Bee_Slic3r"
+program_name = "Bee_Slic3r"
 path_7zip = r"C:\Program Files\7-Zip\7z.exe"
 github_auth_token = "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"
 
