d804a422b86068b404b600ced6204b64360a7bb9
melROLL <60772441+melROLL@users.noreply.github.com>
2022-11-24 14:25:37 +0100

    modified files creat realase and dialog

--- create_release.py
+++ create_release.py
@@ -14,8 +14,8 @@ from datetime import date
 import tarfile
 import subprocess
 
-repo = "supermerill/SuperSlicer"
-program_name = "SuperSlicer"
+repo = "melROL/Prosciutto_Slic3r"
+program_name = "Prosciutto_Slic3r"
 path_7zip = r"C:\Program Files\7-Zip\7z.exe"
 github_auth_token = "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"
 
